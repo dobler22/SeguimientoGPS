15fe13067d1eff263a4296b5d0cb21de1503f43f
dobler22 <jordirr22@gmail.com>
2025-02-10 23:50:15 -0500

    arreglado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: '',
+    path: 'home',
+    redirectTo: 'home',
     pathMatch: 'full'
-
   },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+
 ];
 
 @NgModule({
